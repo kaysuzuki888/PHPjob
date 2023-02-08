2e4ddab89c61f5e2c63f8cc136c2c7b41a2db5af
kaysuzuki888 <115004578+kaysuzuki888@users.noreply.github.com>
2023-02-08 00:04:40 +0000

    Delete index.php

--- 3-3/index.php
+++
@@ -1,5 +0,0 @@
-<span style="font-size:2em";>０〜９の番号を使って数字の羅列を入力してください。</span>
-    <br><br>
-    <form action="result.php" method="get">
-    <input style="font-size:30"; border-radius: 10px: border: 5px solid ♯cccc; type="number" min="0" name="my_numbers">
-    <input style="font-size:30"; type="submit" value="占う"

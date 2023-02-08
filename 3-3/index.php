49b858c5124fd205460c6e2a6b5385064d4130ed
kaysuzuki888 <115004578+kaysuzuki888@users.noreply.github.com>
2023-02-08 00:02:02 +0000

    Update index.php

--- 3-3/index.php
+++ 3-3/index.php
@@ -2,5 +2,4 @@
     <br><br>
     <form action="result.php" method="get">
     <input style="font-size:30"; border-radius: 10px: border: 5px solid ♯cccc; type="number" min="0" name="my_numbers">
-    <input style="font-size:30"; type="submit" value="占う">
-    </form>
+    <input style="font-size:30"; type="submit" value="占う"
